cb96ef2f6ff2c5c03dd056ee1ca4b4b2ae87e82d
Meher <U6020980@ten.thomsonreuters.com>
2016-02-16 10:14:16 -0500

    testing

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Users = require('./models/user');
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + Users;
@@ -112,5 +112,5 @@ userRoute.delete(function(req, res) {
 app.use('/api', router);
 
 // Start the server
-app.listen(port);
-console.log('Insert user on port ' + port);
+app.listen(server_port,server_ip_address);
+console.log('Insert user on port ' + server_port);
